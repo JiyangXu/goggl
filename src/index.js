61dd96b606fa8f430876293cf88490dfb088a130
JiyangXu <xuyang1006@hotmail.com>
2021-11-07 21:01:14 +1100

    change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from "react-dom"
+import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App';
+import './global.css'
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+import {ResultContextProvier} from './contexts/ResultContextProvider'
+
+ReactDOM.render(
+    <ResultContextProvier>
+    <Router>
+        <App/>
+    </Router>
+    </ResultContextProvier>,
+    document.getElementById("root"))
